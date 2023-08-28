1931c49b794f486ceae926c72c319edc4a7b64d9
Srutip04 <subuhishruti75@gmail.com>
2023-08-28 16:58:16 +0530

    starter files

---
+++ src/lectures/exercise/variables/variables.go
@@ -0,0 +1,41 @@
+//Summary:
+//  Print basic information to the terminal using various variable
+//  creation techniques. The information may be printed using any
+//  formatting you like.
+//
+//Requirements:
+
+//Notes:
+//* Use fmt.Println() to print out information
+//* Basic math operations are:
+//    Subtraction    -
+// 	  Addition       +
+// 	  Multiplication *
+// 	  Division       /
+
+package main
+
+import "fmt"
+
+func main() {
+	//* Store your favorite color in a variable using the `var` keyword
+	var favColor = "black"
+	fmt.Println("fav color is",favColor)
+    //* Store your birth year and age (in years) in two variables using
+    //  compound assignment
+     birthYear,age := 2002,21
+	 fmt.Println("Born in",birthYear,"age =",age,"years")
+//* Store your first & last initials in two variables using block assignment
+     var(
+		firstInitial = "S"
+		lastInitial = "P"
+	 )
+	 fmt.Println("Initials=",firstInitial,lastInitial)
+//* Declare (but don't assign!) a variable for your age (in days),
+//  then assign it on the next line by multiplying 365 with the age
+// 	variable created earlier
+//
+    var ageInDays int
+	ageInDays = 365 * age
+	fmt.Println("I'm",ageInDays,"days old")
+}
