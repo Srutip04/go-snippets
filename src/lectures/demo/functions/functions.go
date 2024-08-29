047e462197d6970dfdb0c2e8b28de2cb2801fba7
srutip04 <subuhishruti75@gmail.com>
2024-08-29 19:29:40 +0530

    functions demo

--- src/lectures/demo/functions/functions.go
+++ src/lectures/demo/functions/functions.go
@@ -1,7 +1,24 @@
 package main
 
 import "fmt"
-
+func double(x int) int{
+	return x + x
+}
+func add(lhs,rhs int) int{
+	return lhs + rhs
+}
+func greet(){
+	fmt.Println("hello")
+}
 func main() {
+   greet()
+   dozen := double(6)
+   fmt.Println("Dozen:",dozen)
+
+   bakersDozen := add(dozen,1)
+   fmt.Println("BakerDozen:",bakersDozen)
+
+   another := add(double(6),1)
+   fmt.Println("result",another)
 
 }
