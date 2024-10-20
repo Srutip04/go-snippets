a38adb0d3db9c1d4de5984bfcb04bd3ecbe6f13c
srutip04 <subuhishruti75@gmail.com>
2024-10-20 23:46:51 +0530

    if-else

--- src/lectures/demo/if-else/if-else.go
+++ src/lectures/demo/if-else/if-else.go
@@ -8,5 +8,20 @@ func average(a, b, c int) float32 {
 }
 
 func main() {
+	quiz1, quiz2, quiz3 := 9, 7, 8
+
+	if quiz1 > quiz2 {
+		fmt.Println("quiz1 scored higher")
+	} else if quiz1 < quiz2 {
+		fmt.Println("quiz2 scored higher")
+	} else {
+		fmt.Println("quiz1 & quiz2 have same score")
+	}
+
+	if average(quiz1, quiz2, quiz3) > 7 {
+		fmt.Println("acceptable grades")
+	} else {
+		fmt.Println("Bad JOb!!")
+	}
 
 }
