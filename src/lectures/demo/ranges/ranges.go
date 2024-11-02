d2d3ce3f2d632079c4a537ddc60c6bdd79948b12
srutip04 <subuhishruti75@gmail.com>
2024-11-03 01:04:56 +0530

    ranges & slices

--- src/lectures/demo/ranges/ranges.go
+++ src/lectures/demo/ranges/ranges.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	slice := []string{"Hello","World","!"}
+
+	for i, element := range slice{
+		fmt.Println(i,element,":")
+
+		for _, ch := range element{
+			fmt.Println("   %q\n", ch)
+		}
+	}
 }
